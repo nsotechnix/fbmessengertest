e83a72026b9e4d7f14cf972b7a648630d7ca3eaf
nsotechnix <ns.otechnix@gmail.com>
2022-04-18 13:33:21 +0530

    chat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 // import MessengerCustomerChat from 'react-messenger-customer-chat';
+import { MessengerChat } from 'react-messenger-chat-plugin';
 
 function App() {
   return (
@@ -10,6 +11,21 @@ function App() {
           pageId="915970668548064"
           appId="1103912743784569"
         /> */}
+
+        <MessengerChat
+          pageId='915970668548064'
+          language='en_US'
+          themeColor={'#F2F3G2'}
+          height={24}
+          loggedInGreeting='Hello logged in user!'
+          loggedOutGreeting='Hello stranger!'
+          autoExpand={true}
+          debugMode={false}
+          onMessengerShow={() => {console.log('onMessengerShow')}}
+          onMessengerHide={() => {console.log('onMessengerHide')}}
+          onMessengerDialogShow={() => {console.log('onMessengerDialogShow')}}
+          onMessengerDialogHide={() => {console.log('onMessengerDialogHide')}}
+          />
       </header>
     </div>
   );
