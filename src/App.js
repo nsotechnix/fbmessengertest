65017c3e3487820acb83a5bba959526ef0c17146
nsotechnix <ns.otechnix@gmail.com>
2022-04-18 17:25:02 +0530

    i cant push my code I asked dipak to do it

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
-// import { MessengerChat } from 'react-messenger-chat-plugin';
-// import React from 'react'
+import { MessengerChat } from 'react-messenger-chat-plugin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This is a Chat Page 2.1</h1>
-        <MessengerCustomerChat
-          pageId="915970668548064"
-          appId="1103912743784569"
-        />
-
-        {/* <MessengerChat
+        <h3>Test Chat</h3>
+  
+        <MessengerChat
           pageId='915970668548064'
           language='en_US'
           themeColor={'#F2F3G2'}
@@ -22,11 +16,7 @@ function App() {
           loggedOutGreeting='Hello stranger!'
           autoExpand={true}
           debugMode={false}
-          onMessengerShow={() => {console.log('onMessengerShow')}}
-          onMessengerHide={() => {console.log('onMessengerHide')}}
-          onMessengerDialogShow={() => {console.log('onMessengerDialogShow')}}
-          onMessengerDialogHide={() => {console.log('onMessengerDialogHide')}}
-          /> */}
+        />
       </header>
     </div>
   );
