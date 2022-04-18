7322057df060cce2d1b113501b5d053f75fad574
nsotechnix <ns.otechnix@gmail.com>
2022-04-18 12:22:13 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is a Chat Page</h1>
+        <MessengerCustomerChat
+          pageId="915970668548064"
+          appId="1103912743784569"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
