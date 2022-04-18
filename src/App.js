898769b24e5b80b95133ce86a5a7bc9ccc5c9ec9
nsotechnix <ns.otechnix@gmail.com>
2022-04-18 13:21:38 +0530

    newest change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is a Chat Page</h1>
-        <MessengerCustomerChat
+        {/* <MessengerCustomerChat
           pageId="915970668548064"
           appId="1103912743784569"
-        />
+        /> */}
       </header>
     </div>
   );
