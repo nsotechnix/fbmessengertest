6cada9ad452d0f7578e20c439fec29db942267c9
nsotechnix <90454422+nsotechnix@users.noreply.github.com>
2022-04-27 16:28:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3>Test Chat</h3>
-  
-        <MessengerChat
-          pageId='915970668548064'
-          language='en_US'
-          themeColor={'#F2F3G2'}
-          height={24}
-          loggedInGreeting='Hello logged in user!'
-          loggedOutGreeting='Hello stranger!'
-          autoExpand={true}
-          debugMode={false}
-        />
       </header>
     </div>
   );
